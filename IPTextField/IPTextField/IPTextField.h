154647a0b84ef631495863bbdff1726b35c2cf1c
Mariana Samardzic <marianasamardzic2@gmail.com>
2020-11-24 11:40:32 +0100

    initial project setup

---
+++ IPTextField/IPTextField/IPTextField.h
@@ -0,0 +1,18 @@
+//
+//  IPTextField.h
+//  IPTextField
+//
+//  Created by Mariana Samardzic on 24/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IPTextField.
+FOUNDATION_EXPORT double IPTextFieldVersionNumber;
+
+//! Project version string for IPTextField.
+FOUNDATION_EXPORT const unsigned char IPTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IPTextField/PublicHeader.h>
+
+
