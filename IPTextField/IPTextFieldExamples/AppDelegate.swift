e01a1fdeca4d815c092e602cbf426571bdaf58ae
Mariana Samardzic <marianasamardzic2@gmail.com>
2020-11-24 12:26:46 +0100

    added exmple project

---
+++ IPTextField/IPTextFieldExamples/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  IPTextFieldExamples
+//
+//  Created by Mariana Samardzic on 24/11/2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ IPTextField/IPTextFieldExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPTextFieldExamples
+//
+//  Created by Mariana Samardzic on 24/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
